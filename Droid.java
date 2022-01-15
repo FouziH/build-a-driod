07945cbb0d11af484ffa0a44d757ac07cccf6222
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-14 22:02:17 -0600

    ....

--- Droid.java
+++ Droid.java
@@ -1,5 +1,15 @@
 public class Droid {
+
+    String name;
+    int batteryLevel;
+     Droid(String droidName) {
+         
+         name = droidName;
+         batteryLevel = 100;
+
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
