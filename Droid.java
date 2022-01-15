004168aa9c8373a3a05291147887068bc79fd96a
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-14 22:12:02 -0600

    ....

--- Droid.java
+++ Droid.java
@@ -11,6 +11,10 @@ public class Droid {
     public String toString() {
         return "Hello, I'm the droid: " + name;
     }
+    public void performTask(String task) {
+        System.out.println(name + " is performing task: " + task);
+
+    }
 
     public static void main(String[] args) {
 
